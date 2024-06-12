21d1cf9a837955f2ad7191b5d090a0729753020b
Michu01 <jakubmichalski01@gmail.com>
2024-06-12 13:15:51 +0200

    Dodaj pliki projektów.

---
+++ Api/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
