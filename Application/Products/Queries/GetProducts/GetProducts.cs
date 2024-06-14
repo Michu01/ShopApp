67cedd10764af4eb22e937c36c55ffe77e0b5447
Michu01 <jakubmichalski01@gmail.com>
2024-06-14 21:09:23 +0200

    Add product search

--- Application/Products/Queries/GetProducts/GetProducts.cs
+++ Application/Products/Queries/GetProducts/GetProducts.cs
@@ -12,15 +12,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Products.Queries.GetProducts;
 
-public record GetProductsQuery(int Page = 1, int Limit = 30) : IRequest<PaginatedResult<Product>>;
+public record GetProductsQuery(int Page = 1, int Limit = 30, string? Search = null) : IRequest<PaginatedResult<Product>>;
 
 public class GetProductsQueryHandler(IApplicationDbContext dbContext, IMapper mapper) : IRequestHandler<GetProductsQuery, PaginatedResult<Product>>
 {
     public async Task<PaginatedResult<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
     {
-        var result = await dbContext
+        var query = dbContext
             .Products
-            .AsNoTracking()
+            .AsNoTracking();
+
+        if (!string.IsNullOrEmpty(request.Search))
+        {
+            var words = request.Search.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            query = query.Where(product => words.Any(word => product.Name.Contains(word)));
+        }
+            
+        var result = await query
             .ProjectTo<Product>(mapper.ConfigurationProvider)
             .ToPaginatedResult(request.Page, request.Limit, cancellationToken);
 
